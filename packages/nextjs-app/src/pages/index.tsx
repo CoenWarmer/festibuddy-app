09d27518439f3f73fa43cf8b63a21bb8fbaa34d9
Coen Warmer <coenwarmer@Coens-MacBook-Pro.local>
2022-07-27 15:58:40 +0200

    :sparkles: Add Prettier

--- packages/nextjs-app/src/pages/index.tsx
+++ packages/nextjs-app/src/pages/index.tsx
@@ -1,24 +1,25 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery([
-    "example.hello",
-    { text: "from tRPC" },
+    'example.hello',
+    { text: 'from tRPC' },
   ]);
 
   return (
     <>
       <Head>
         <title>Festibuddy</title>
-        <meta name="description" content="The fun way to experience a festival" />
+        <meta
+          name="description"
+          content="The fun way to experience a festival"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-
-        <div>
-        </div>
+        <div></div>
       </div>
     </>
   );
